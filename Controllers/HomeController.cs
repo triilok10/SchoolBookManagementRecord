e03f766a48fd6b543a16bf1c2d0b22a22ca074d2
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-03-20 17:47:37 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolBookManagementRecord.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+        public ActionResult Accounts() 
+        { 
+            return View();        
+        }
+        public ActionResult Students() 
+        { 
+            return View();
+        }
+        public ActionResult Teachers() 
+        { 
+            return View();
+        }
+        public ActionResult Notices()
+        {
+            return View();
+        }
+        public ActionResult Attendance() 
+        { 
+            return View();
+        }
+        public ActionResult Classes() 
+        { 
+            return View();
+        }
+        public ActionResult Transport()
+        {
+            return View();
+        }
+        public ActionResult Departments() 
+        {
+            return View();
+        }
+    }
+}

92adf22c689dbc47cf47554ae655cb762871c049
Trilok Chand Swami <ittiku3@gmail.com>
2024-03-24 17:27:30 +0530

    Done Some Changes to build the Application

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SchoolBookManagementRecord
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Students", id = UrlParameter.Optional }
             );
         }
     }
